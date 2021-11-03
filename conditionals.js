ffd21430401c2311a2d424fa1cbb86e6dd07a61a
mbbaltaji <baltaji@uwindsor.ca>
2021-11-03 18:40:35 +0000

    Use conditionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+const raining = true;
+const cold = false;
+
+if (raining){
+  console.log("Don't forget to bring an umbrella!");
+}
+
+if (cold) {
+  console.log("Don't forget to bring a scarf!");
+}
+
+console.log("Now you are ready to go outside!");
